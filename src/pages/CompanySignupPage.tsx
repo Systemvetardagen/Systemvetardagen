80dd6be1fa6e93d176790b34837c4d7107ffd996
Durelius <wilhelm@durelius.se>
2025-09-03 15:48:24 +0200

    sign up temp page live

--- src/pages/CompanySignupPage.tsx
+++ src/pages/CompanySignupPage.tsx
@@ -8,20 +8,20 @@ interface CompanySignupProps {
 const CompanySignup: React.FC<CompanySignupProps> = () => {
   const { t } = useTranslation();
   return (
-    <div className="bg-background min-h-screen">
+    <div className="bg-background min-h-screen gradient-background">
       <div className="flex flex-col items-center py-32 px-10">
-        <h2 className="text-5xl font-semibold lg:text-6xl mb-8">
+        <h2 className="text-5xl  text-white font-semibold lg:text-6xl mb-8">
           {t("Sign up")}
         </h2>
         <div className="relative">
           <div className="flex items-center flex-wrap justify-center gap-2 mb-4">
-            <h2 className="font-light text-gray-700">{t("Attend our fair")}</h2>
+            <h2 className="font-light  text-white">{t("Attend our fair")}</h2>
             {/* Add any additional elements here if needed */}
           </div>
 
           {/* Signup card */}
-          <div className="max-w-md mx-auto mt-12 p-6 bg-white shadow-lg rounded-lg border border-gray-200">
-            <p className="text-center text-gray-800 text-lg font-medium">
+          <div className="max-w-md mx-auto mt-12 p-6  rounded-lg border  shadow-2xl border-gray-200">
+            <p className="text-center text-white text-lg opacity-100 font-medium">
               If you’re interested in signing up, email us at{" "}
               <a
                 href="mailto:systemvetardagen@disk.su.se"
