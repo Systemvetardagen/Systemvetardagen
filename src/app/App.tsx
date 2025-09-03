689dc464e8cfa606d603934d23ac6ca006a188bc
Fredrik Etsare <fredrik@etsare.se>
2025-09-03 22:28:27 +0200

    router rename

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import Router from "./Routes";
+import Router from "./Router";
 
 const App: React.FC = () => {
   const queryClient = new QueryClient();
