880d7ea82511f280a355ad9090069ab4014af450
Fredrik Etsare <fredrik@etsare.se>
2025-09-11 17:53:33 +0200

    switched to correct iso 639 code for swedish

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -1,23 +1,26 @@
 import common_en from "@/lib/locales/en/common.json";
-import common_se from "@/lib/locales/se/common.json";
+import common_sv from "@/lib/locales/sv/common.json";
 import landing_en from "@/lib/locales/en/landing.json";
-import landing_se from "@/lib/locales/se/landing.json";
+import landing_sv from "@/lib/locales/sv/landing.json";
 import visitInfo_en from "@/lib/locales/en/visit-info.json";
-import visitInfo_se from "@/lib/locales/se/visit-info.json";
+import visitInfo_sv from "@/lib/locales/sv/visit-info.json";
 import about_en from "@/lib/locales/en/about.json";
-import about_se from "@/lib/locales/se/about.json";
+import about_sv from "@/lib/locales/sv/about.json";
 import companies_en from "@/lib/locales/en/companies.json";
-import companies_se from "@/lib/locales/se/companies.json";
+import companies_sv from "@/lib/locales/sv/companies.json";
 import faq_en from "@/lib/locales/en/faq.json";
-import faq_se from "@/lib/locales/se/faq.json";
+import faq_sv from "@/lib/locales/sv/faq.json";
 import navbar_en from "@/lib/locales/en/navbar.json";
-import navbar_se from "@/lib/locales/se/navbar.json";
+import navbar_sv from "@/lib/locales/sv/navbar.json";
 import lectures_en from "@/lib/locales/en/lectures.json";
-import lectures_se from "@/lib/locales/se/lectures.json";
+import lectures_sv from "@/lib/locales/sv/lectures.json";
 import project_group_en from "@/lib/locales/en/project-group.json";
-import project_group_se from "@/lib/locales/se/project-group.json";
+import project_group_sv from "@/lib/locales/sv/project-group.json";
 import programs_en from "@/lib/locales/en/programs.json";
-import programs_se from "@/lib/locales/se/programs.json";
+import programs_sv from "@/lib/locales/sv/programs.json";
+import footer_en from "@/lib/locales/en/footer.json";
+import footer_sv from "@/lib/locales/sv/footer.json";
+
 
 
 import i18next from "i18next";
@@ -41,19 +44,21 @@ i18next.init({
       navbar: navbar_en,
       lectures: lectures_en,
       projectGroup: project_group_en,
-      programs: programs_en
+      programs: programs_en,
+      footer: footer_en
     },
     sv: {
-      common: common_se,
-      landing: landing_se,
-      visitInfo: visitInfo_se,
-      about: about_se,
-      companies: companies_se,
-      faq: faq_se,
-      navbar: navbar_se,
-      lectures: lectures_se,
-      projectGroup: project_group_se,
-      programs: programs_se
+      common: common_sv,
+      landing: landing_sv,
+      visitInfo: visitInfo_sv,
+      about: about_sv,
+      companies: companies_sv,
+      faq: faq_sv,
+      navbar: navbar_sv,
+      lectures: lectures_sv,
+      projectGroup: project_group_sv,
+      programs: programs_sv,
+      footer: footer_sv
     },
   },
 });
