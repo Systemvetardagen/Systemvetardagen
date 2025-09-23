c2497fe1b82f7b87355a3ea635f45c6c1891f605
Fredrik Etsare <Fredrik@etsare.se>
2025-09-23 17:40:38 +0200

    changed backend url

--- src/lib/api/config.ts
+++ src/lib/api/config.ts
@@ -5,7 +5,7 @@ interface GlobalConfig {
 const config: GlobalConfig = {
   apiBaseUrl:
     import.meta.env.PROD === true
-      ? "https://mitt.systemvetardagen.se/api/vi"
+      ? "https://svd.disk.su.se/api/vi"
       : "http://localhost:8080/api/v1",
 };
 
