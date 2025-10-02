6a9f832a72afed80b8b67abbe038fbc11379844f
Fredrik Etsare <fredrik@etsare.se>
2025-10-02 23:15:16 +0200

    rollup manual chunking to increase loadspeed (hopefully 🤓)

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from "@tailwindcss/vite"
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwindcss from "@tailwindcss/vite";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: [
+            "react",
+            "react-dom",
+            "react-router-dom",
+            "react-i18next",
+            "i18next",
+          ],
+          animations: ["gsap", "motion", "@fireworks-js/react"],
+          fetching: ["axios", "@tanstack/react-query"],
+        },
+      },
     },
   },
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
