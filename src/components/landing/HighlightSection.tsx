7d31561b7e45b841789325597899dcfe4f74cef3
Fredrik Etsare <Fredrik@etsare.se>
2025-12-02 17:58:39 +0100

    new heads image

--- src/components/landing/HighlightSection.tsx
+++ src/components/landing/HighlightSection.tsx
@@ -60,7 +60,7 @@ export const HighlightSection: React.FC = () => {
         <BlogPostPreview
           postKey="posts.announcement"
           postId="announcement"
-          imageSrc="/images/heads.jpeg"
+          imageSrc="/images/heads.jpg"
         />
         <div className="mt-8 flex justify-center">
           <NavLink
